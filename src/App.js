2b5901f0b69adbbf1065c10a5dcc2a846216c7af
bill0077 <bill007tjr@gmail.com>
2024-03-01 21:23:27 +0900

    feat: add basic server connection via Socket.IO
    implement basic server connection via Socket.IO client.
    setup basic project structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { socket } from './socket';
+import { ConnectionManager } from './components/ConnectionManager';
+import { MyForm } from './components/MyForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [message, setMessage] = useState([]);
+  const [usersOnline, setUsersOnline] = useState([]);
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    function onMsgRecieved(value) {
+      setMessage(previous => [...previous, value]);
+    }
+
+    function onUsersOnline(value) {
+      setUsersOnline(value);
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+    socket.on('msgRecieved', onMsgRecieved);
+    socket.on('usersOnline', onUsersOnline);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+      socket.off('msgRecieved', onMsgRecieved);
+      socket.off('usersOnline', onUsersOnline);
+    };
+  }, []);
+
+  if (isConnected) {
+    return (
+      <div className="App">
+        <p>State: { '' + isConnected }</p>
+        <p>online: {usersOnline}</p>
+        <ul>
+        {
+          message.map((event, index) =>
+            <li key={ index }>{ event.data }</li>
+          )
+        }
+        </ul>
+        <ConnectionManager />
+        <MyForm />
+      </div>
+    );  
+  } else {
+    return (
+      <div className="App">
+        <p>State: { '' + isConnected }</p>
+        <ConnectionManager />
+      </div>
+    );
+  }
 }
-
-export default App;
